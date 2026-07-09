8866f75c02b682b2e2cd19290572b10b3489c9e3
Clayton Ramsey <claytonwramsey@gmail.com>
2026-07-09 17:09:23 -0500

    fix: special case for u64 index to hide warnings

--- crates/mvtable/src/lib.rs
+++ crates/mvtable/src/lib.rs
@@ -300,8 +300,21 @@ macro_rules! impl_index {
 impl_index!(u8);
 impl_index!(u16);
 impl_index!(u32);
-impl_index!(u64);
 impl_index!(usize);
+
+// special case to suppress warnings
+impl Index for u64 {
+    const ZERO: Self = 0;
+    const SENTINEL: Self = Self::MAX;
+    fn from_usize(x: usize) -> Option<Self> {
+        let v = Self::try_from(x).ok()?;
+        (v != Self::SENTINEL).then_some(v)
+    }
+    #[expect(clippy::cast_possible_truncation)]
+    fn to_usize(self) -> usize {
+        self as usize
+    }
+}
 
 /// An axis-aligned bounding box, used both as a global bound on the point cloud and as a local
 /// bound on the points contained by a single voxel.
